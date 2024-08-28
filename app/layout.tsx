f7abdd8a4bf8eb660337e502e16562925d325e1a
Bhishman <desaibhishman1@gmail.com>
2024-08-28 14:43:47 -0300

    feat: adding google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import { Metadata } from "next";
+import Script from "next/script";
+
 
 export const metadata: Metadata = {
   title: "Bhishman's Blog",
@@ -17,12 +19,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="dark:bg-slate-800">
-        <Navbar />
-        <main className="px-4 md:px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
-          {children}
-        </main>
-      </body>
+        <head>
+            <Script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=G-6V5PRQ47BR"
+            ></Script>
+            <Script id="google-analytics">
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                
+                gtag('config', 'G-6V5PRQ47BR');
+              `}
+            </Script>
+        </head>
+        <body className="dark:bg-slate-800">
+            <Navbar/>
+            <main className="px-4 md:px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+                {children}
+            </main>
+        </body>
     </html>
   );
 }
